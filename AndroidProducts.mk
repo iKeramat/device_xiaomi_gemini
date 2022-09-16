0c16b09ad89b97b565905f2dc6f5e463808fccb9
Mohammad Hasan Keramat J <ikeramat@protonmail.com>
2022-09-16 11:22:21 +0430

    msm8996-common: bringup lmodroid

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_gemini.mk
+    $(LOCAL_DIR)/lmodroid_gemini.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_gemini-user \
-    lineage_gemini-userdebug \
-    lineage_gemini-eng
+    lmodroid_gemini-user \
+    lmodroid_gemini-userdebug \
+    lmodroid_gemini-eng
